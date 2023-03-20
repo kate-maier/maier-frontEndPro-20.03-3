9e799fbfccdd76661aad420ddc117241be03999b
AdminK <volvenkova.k@gmail.com>
2023-03-20 17:22:20 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let numberOneString = prompt("Please, enter the first number");
+let numberTwoString = prompt("Please, enter the second number");
+let numberThreeString = prompt("Please, enter the third number");
+let numberOne = Number(numberOneString);
+let numberTwo = Number(numberTwoString);
+let numberThree = Number(numberThreeString);
+let averageValue = (numberOne + numberTwo + numberThree)/3;
+alert(averageValue);
